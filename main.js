a018b169b9ec87436f227c93bcab7408ab7abf2c
sanjayshiva1234 <87415115+sanjayshiva1234@users.noreply.github.com>
2021-07-22 10:04:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var Image = [
+"https://s3-projectsassets.whjr.online/Monkey_01.png",
+ "https://s3-projectsassets.whjr.online/Monkey_02.png",
+  "https://s3-projectsassets.whjr.online/Monkey_03.png",
+   "https://s3-projectsassets.whjr.online/Monkey_04.png",
+    "https://s3-projectsassets.whjr.online/Monkey_05.png",
+    "https://s3-projectsassets.whjr.online/Monkey_06.png",
+   "https://s3-projectsassets.whjr.online/Monkey_07.png",
+  "https://s3-projectsassets.whjr.online/Monkey_08.png",
+   "https://s3-projectsassets.whjr.online/Monkey_09.png",
+    "https://s3-projectsassets.whjr.online/Monkey_10.png",
+];
+var i = 0;
+
+function nextslide() {
+    document.getElementById("album1").src = Image[i];
+}
+if (i == 9) {
+    i = 0;
+}
